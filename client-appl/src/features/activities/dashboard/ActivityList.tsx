65e48518ebb70c6ab98f2e1117a856a8c00ebf70
James Lee <ejlee@vscnet.com>
2022-09-28 22:55:00 -0700

    Separating each activity into 4 segments

--- client-appl/src/features/activities/dashboard/ActivityList.tsx
+++ client-appl/src/features/activities/dashboard/ActivityList.tsx
@@ -1,6 +1,6 @@
 import { observer } from 'mobx-react-lite';
 import React, { Fragment } from 'react';
-import { Item, Segment } from 'semantic-ui-react';
+import { Header, Item, Segment } from 'semantic-ui-react';
 import { useStore } from '../../../app/stores/store';
 import ActivitySublist from './ActivitySublist';
 
@@ -9,17 +9,17 @@ export default observer(function ActivityList() {
     const { groupedActivities } = activityStore;
 
     return (
-        <Segment>
-            <Item.Group divided>
-                {groupedActivities.map(([date, activities]) =>
-                    <Fragment key={date}>
-                        <h4>Date of Activity(s): {date}</h4>
-                        {activities.map(activity =>
-                            <ActivitySublist key={activity.id} activity={activity} />
-                        )}
-                    </Fragment>
-                )}
-            </Item.Group>
-        </Segment>
+        <>
+            {groupedActivities.map(([date, activities]) =>
+                <Fragment key={date}>
+                    <h4 style={{ color: "teal" }} >{date}</h4>
+                    <>
+                            {activities.map(activity =>
+                                    <ActivitySublist key={activity.id} activity={activity} />
+                            )}
+                    </>
+                </Fragment>
+            )}
+        </>
     )
 })
--- client-appl/src/features/activities/dashboard/ActivitySublist.tsx
+++ client-appl/src/features/activities/dashboard/ActivitySublist.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Button, Item, Label } from 'semantic-ui-react';
+import { Button, Icon, Item, Label, Segment } from 'semantic-ui-react';
 import { Activity } from '../../../app/models/activity';
 import { useStore } from '../../../app/stores/store';
 
@@ -8,7 +8,7 @@ interface Props {
     activity: Activity
 }
 
-function ReactComponent({ activity }: Props) {
+function ActivitySublist({ activity }: Props) {
 
     const { activityStore } = useStore();
     const { loading, deleteActivity } = activityStore;
@@ -20,31 +20,34 @@ function ReactComponent({ activity }: Props) {
         deleteActivity(id);
     }
 
-return (
-        <Item>
-            <Item.Image src={`/assets/categoryImages/${activity?.category}.jpg`} />
-            <Item.Content>
-                <Item.Header as='a'>{activity.title}</Item.Header>
-                <Item.Meta>{activity.date}</Item.Meta>
-                <Item.Description>
-                    <div>{activity.description}</div>
-                    <div>{activity.city}, {activity.venue}</div>
-                </Item.Description>
-                <Item.Extra>
-                    <Button as={Link} to={`/activities/${activity.id}`} floated='right' color='blue' content='View' />
-                    <Button
-                        name={activity.id}
-                        loading={loading && target === activity.id}
-                        onClick={() => handleDelete(activity.id)}
-                        floated='right'
-                        content='Delete'
-                        color='red'
-                    />
-                    <Label basic content={activity.category} />
-                </Item.Extra>
-            </Item.Content>
-        </Item>
+    return (
+        <Segment.Group>
+            <Segment>
+                <Item.Group>
+                    <Item>
+                        <Item.Image size="medium" src={`/assets/categoryImages/${activity?.category}.jpg`} />
+                        <Item.Content>
+                            <Item.Header as='a'>{activity.title}</Item.Header>
+                            <Item.Meta>{activity.date}</Item.Meta>
+                        </Item.Content>
+                    </Item>
+                </Item.Group>
+            </Segment>
+
+            <Segment>
+                <span>
+                    <Icon name="clock outline" />{activity.date}  <Icon name="map marker alternate" />{activity.venue}, {activity.city}
+                </span>
+            </Segment>
+            <Segment secondary>
+                Attendees
+            </Segment>
+            <Segment clearing>
+                <span>{activity.description}</span>
+                <Button as="Link" to="/activities/{activity.id}" color="teal" floated="right" content="View" />
+            </Segment>
+        </Segment.Group>
     );
 }
 
-export default ReactComponent;
+export default ActivitySublist;
